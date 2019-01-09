df83c1c8dc253a67779d786517633b6c9a6c160d
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-01-09 13:22:25 +0100

    Forward declaration of energy_expression

--- src/AngularMomentumAlgebra.jl
+++ src/AngularMomentumAlgebra.jl
@@ -18,4 +18,13 @@ include("couplings.jl")
 
 @gen_compare_false OverlapIntegral DiagonalIntegral GeneralSlaterIntegral DirectSlaterIntegral ExchangeSlaterIntegral SlaterPotential Bra Ket Braket LagrangeMultiplier AngularRoot SumVariable IIIJ VIJ IXJ
 
+# Forward declaration, to be implemented on case-by-case basis by
+# other packages. Should return a
+# Pair{symmetry,AbstractMatrix{EnergyExpression}} (for a single
+# argument) or a vector of such (for a vector argument).
+energy_expression(::Any; verbosity=0) =
+    error("Not implemented")
+
+export energy_expression
+
 end # module
