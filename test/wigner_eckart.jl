24ad7364f17b2822af60a32edf5b62be0f9c1c3f
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-10-11 09:20:33 +0200

    Package updates

--- test/wigner_eckart.jl
+++ test/wigner_eckart.jl
@@ -8,8 +8,8 @@ using AngularMomentumAlgebra.Dipoles
 
         @testset "z" begin
             for o in os
-                ms = mℓrange(o)
-                ℓ = ms[end]
+                ℓ = o.ℓ
+                ms = -ℓ:ℓ
                 for m in ms
                     @test wigner_eckart(ℓ,m,z,ℓ+1,m) ≈ √((ℓ+m+1)*(ℓ-m+1)/((2ℓ+3)*(2ℓ+1)))
                 end
@@ -18,8 +18,8 @@ using AngularMomentumAlgebra.Dipoles
 
         @testset "x" begin
             for o in os
-                ms = mℓrange(o)
-                ℓ = ms[end]
+                ℓ = o.ℓ
+                ms = -ℓ:ℓ
                 for m in ms
                     # TODO Figure out where the minus sign comes from
                     @test wigner_eckart(ℓ,m,x,ℓ+1,m+1) ≈ -0.5*√((ℓ+m+2)*(ℓ+m+1)/((2ℓ+3)*(2ℓ+1)))
