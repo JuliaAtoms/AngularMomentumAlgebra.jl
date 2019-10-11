e9a39e68e2d9e8a33745267128b9175dcd5d32c0
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-10-11 09:14:23 +0200

    Support for rectangular energy expressions

--- src/energy_expressions.jl
+++ src/energy_expressions.jl
@@ -25,6 +25,34 @@ coordinates (see [`multipole_expand`](@ref)).
 integrate_spinor(me::OrbitalMatrixElement{2,<:Any,<:CoulombInteraction,<:Any}) =
     multipole_expand(me)
 
+
+"""
+    integrate_spinor(integral::NBodyTermFactor)
+
+Dummy method that returns `integral` unchanged, used for all
+`NBodyTermFactor`s that are _not_ to be multipole-expanded.
+"""
+integrate_spinor(integral::NBodyTermFactor) = NBodyMatrixElement([integral])
+
+"""
+    Matrix(a::Vector{<:Configuration{<:SpinOrbital}},
+           op::QuantumOperator,
+           b::Vector{<:Configuration{<:SpinOrbital}}[, overlaps])
+
+Generate the energy-expression associated with the quantum operator
+`op`, in the basis of the spin-configurations `a` and `b`, with an
+optional set of orbital `overlaps`, specifying any desired
+non-orthogonalities. The energy expression is generated in a
+basis-agnostic way by EnergyExpressions.jl and each term is then
+integrated over the spin-angular coordinates using
+[`integrate_spinor`](@ref).
+"""
+function Base.Matrix(a::VSC, op::QuantumOperator, b::VSC,
+                     overlaps::Vector{<:OrbitalOverlap}=OrbitalOverlap[]) where {VSC<:AbstractVector{<:Configuration{<:SpinOrbital}}}
+    E = Matrix(SlaterDeterminant.(a), op, SlaterDeterminant.(b), overlaps)
+    transform(integrate_spinor, E)
+end
+
 """
     Matrix(op::QuantumOperator,
            spin_cfgs::Vector{<:Configuration{<:SpinOrbital}}[, overlaps])
@@ -37,19 +65,6 @@ basis-agnostic way by EnergyExpressions.jl and each term is then
 integrated over the spin-angular coordinates using
 [`integrate_spinor`](@ref).
 """
-function Base.Matrix(op::QuantumOperator, spin_cfgs::VSC,
-                     overlaps::Vector{<:OrbitalOverlap}=OrbitalOverlap[]) where {VSC<:AbstractVector{<:Configuration{<:SpinOrbital}}}
-    M = Matrix(op, SlaterDeterminant.(spin_cfgs), overlaps)
-    m,n = size(M)
-    Mm = spzeros(eltype(M), m, n)
-    for i = 1:m
-        for j = 1:n
-            # println(repeat("=", 100))
-            # @show i,j
-            me = transform(integrate_spinor, M[i,j])
-            iszero(me) && continue
-            Mm[i,j] = me
-        end
-    end
-    Mm
-end
+Base.Matrix(op::QuantumOperator, spin_cfgs::VSC,
+            overlaps::Vector{<:OrbitalOverlap}=OrbitalOverlap[]) where {VSC<:AbstractVector{<:Configuration{<:SpinOrbital}}} =
+                Matrix(spin_cfgs, op, spin_cfgs, overlaps)
