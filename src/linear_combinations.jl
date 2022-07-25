af0cc5d656feba4f34e714d1e785c030d361da2f
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2022-07-25 09:24:07 +0200

    Test LinearCombination

--- src/linear_combinations.jl
+++ src/linear_combinations.jl
@@ -26,6 +26,8 @@ Base.hash(lc::LinearCombination, h::UInt) =
 Base.zero(::LinearCombination{T,N}) where {T,N} =
     LinearCombination(Vector{T}(), Vector{N}())
 
+Base.iszero(lc::LinearCombination) = isempty(lc.Ts)
+
 Base.getindex(lc::LinearCombination, i) =
     (lc.Ts[i],lc.coeffs[i])
 
@@ -36,7 +38,6 @@ function Base.show(io::IO, lc::LinearCombination)
     n = length(lc)
     for (i,(t,c)) in enumerate(lc)
         showcoeff(io, c, i > 1)
-        write(io, " ")
         show(io, t)
         i < n && write(io, " ")
     end
@@ -53,6 +54,12 @@ Base.:(+)(A::LC, B::T) where {T,N,LC<:LinearCombination{<:T,N}} =
 
 Base.:(+)(A::T, B::LC) where {T,N,LC<:LinearCombination{<:T,N}} =
     LinearCombination(vcat(A, B.Ts), vcat(one(N), B.coeffs))
+
+Base.:(-)(A::LC, B::T) where {T,N,LC<:LinearCombination{<:T,N}} =
+    A + (-1)*B
+
+Base.:(-)(A::T, B::LC) where {T,N,LC<:LinearCombination{<:T,N}} =
+    A + (-1)*B
 
 Base.:(-)(A::LC) where {LC<:LinearCombination} =
     LinearCombination(A.Ts, -1 * A.coeffs)
